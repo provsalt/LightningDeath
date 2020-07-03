0604aa15de8b3efe5d4dde8d5a769e07cf3b9d76
provsalt <gooraymond39@gmail.com>
2020-07-03 16:34:57 +0800

    Cleanup config code

--- src/provsalt/lightningdeath/Loader.php
+++ src/provsalt/lightningdeath/Loader.php
@@ -4,15 +4,13 @@ namespace provsalt\lightningdeath;
 
 use JackMD\UpdateNotifier\UpdateNotifier;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\Config;
 
 class Loader extends PluginBase {
-    public $cfg;
+
     public function onEnable() :void{
         $this->getServer()->getPluginManager()->registerEvents(new LightningListener($this), $this);
-        $this->saveResource("config.yml");
-        $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-        if ($this->cfg->get("version") !== 1){
+        $this->saveDefaultConfig();
+        if ($this->getConfig()->get("version") !== 1){
             $this->getLogger()->critical("Please regenerate your config file!");
             $this->getServer()->getPluginManager()->disablePlugin($this);
         }
