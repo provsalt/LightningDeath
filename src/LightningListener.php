55abc98f30523f4fbf694cebb82ed931eec9eb92
Raymond <raymond.goo124@gmail.com>
2022-12-24 03:23:09 +0800

    plugin.yml: Update to the latest API version

--- src/LightningListener.php
+++ src/LightningListener.php
@@ -6,6 +6,7 @@ use pocketmine\block\Block;
 use pocketmine\entity\Entity;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\network\mcpe\protocol\types\entity\PropertySyncData;
 use pocketmine\world\particle\BlockBreakParticle;
 use pocketmine\math\Vector3;
 use pocketmine\network\mcpe\protocol\AddActorPacket;
@@ -35,9 +36,17 @@ class LightningListener implements Listener {
 	public function Lightning(Player $player) :void{
 		if(in_array($player->getWorld()->getFolderName(), $this->getOwner()->getConfig()->get("worlds"))){
 			$pos = $player->getPosition();
-			$light2 = AddActorPacket::create(Entity::nextRuntimeId(), 1, "minecraft:lightning_bolt", $player->getPosition()->asVector3(), null, $player->getLocation()->getYaw(), $player->getLocation()->getPitch(), 0.0, 0.0, [], [], []);
+            $light2 = new AddActorPacket();
+            $light2->actorUniqueId = Entity::nextRuntimeId();
+            $light2->actorRuntimeId = 1;
+            $light2->position = $player->getPosition()->asVector3();
+            $light2->type = "minecraft:lightning_bolt";
+            $light2->yaw = $player->getLocation()->getYaw();
+            $light2->syncedProperties = new PropertySyncData([], []);
+
 			$block = $player->getWorld()->getBlock($player->getPosition()->floor()->down());
 			$particle = new BlockBreakParticle($block);
+
 			$player->getWorld()->addParticle($pos, $particle, $player->getWorld()->getPlayers());
 			$sound2 = PlaySoundPacket::create("ambient.weather.thunder", $pos->getX(), $pos->getY(), $pos->getZ(), 1, 1);
 			Server::getInstance()->broadcastPackets($player->getWorld()->getPlayers(), [$light2, $sound2]);
